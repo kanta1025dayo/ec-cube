2464587c5700153eafb4eeb448b4838befa21951
k-nakasone <enaioshi407@gmail.com>
2024-04-16 10:46:46 +0900

    えらーlogだす

--- src/Eccube/Form/Type/Front/EntryType.php
+++ src/Eccube/Form/Type/Front/EntryType.php
@@ -80,6 +80,7 @@ class EntryType extends AbstractType
                 'constraints' => [
                     new Assert\NotBlank(['message' => '携帯電話入力']),
                     new Assert\Regex([
+                        phpinfo(),
                         'pattern' => '/^(\+81\-?|\(0\))?9[0|1|7|8|9](\-?\d{4}){2}$/',
                         'match' => false,
                         'message' => '日本国内の携帯番号を入力してください。'
